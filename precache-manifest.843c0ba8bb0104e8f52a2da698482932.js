35fa82829ca1ddcdbd7ae90c5d8405d4e687b52c
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-10-13 12:46:08 +0200

    Updates

---
+++ precache-manifest.843c0ba8bb0104e8f52a2da698482932.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b04ff2e7865722152e90be6572904b98",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "65d872be9f02fa8a4842",
+    "url": "/weather-react-app/static/css/main.a37de799.chunk.css"
+  },
+  {
+    "revision": "e35a6527f472fe3c98c4",
+    "url": "/weather-react-app/static/js/2.91f7ad1f.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/weather-react-app/static/js/2.91f7ad1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65d872be9f02fa8a4842",
+    "url": "/weather-react-app/static/js/main.95b2c6c2.chunk.js"
+  },
+  {
+    "revision": "80a11080e1d5d2b66f46",
+    "url": "/weather-react-app/static/js/runtime-main.42a25d67.js"
+  }
+]);
